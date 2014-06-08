46fcf650d7fb07c5386f39df3292016957f78f7b
V <vlall@users.noreply.github.com>
2014-06-08 03:27:22 -0400

    Create ObjectTutor.swift

---
+++ ObjectTutor.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+let someResolution = VideoMode()
+println(someResolution.frameRate)
