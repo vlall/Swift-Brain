e478801db3be2cf38edf2159fe66abc5aff17299
V <vlall@users.noreply.github.com>
2014-06-18 01:33:28 -0400

    Create NeuralNet.swift

---
+++ NeuralNet.swift
@@ -0,0 +1,17 @@
+// Creating a Neural Network from scratch
+
+import Foundation
+func randomFunc(a: Double, b:Double) -> (Double) {
+    var randNum = arc4random_uniform(101)
+    var output = (b-a)*Double(randNum) + (a)
+    return output
+}
+
+func randomFunc(I:Double, J:Double)->(ArrayType){
+    var fill = 0.0
+    var m: Integer[] = []
+    for index in 1...I{
+        m.append([fill]*J)
+    return m
+    }
+}
