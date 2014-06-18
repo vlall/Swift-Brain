3381ae188a8c1a4e692576978443d8a175f3ede6
V <vlall@users.noreply.github.com>
2014-06-18 02:38:33 -0400

    Create matrix.swift

---
+++ matrix.swift
@@ -0,0 +1,11 @@
+// Some notes. This doesn't make sense.
+
+func MatrixMaker(I:Double, J:Double)->(Array<Array<Double>>){
+    var NumColumns = 27
+    var NumRows = 52
+    var array = Array<Array<Double>>()
+    for column in 0..NumColumns {
+        array.append(Array(count:NumRows, repeatedValue:Double()))
+    }
+    return Matrix
+}
