0f36167296723fe3a0a9d7c015a661d0a7115470
V <vlall@users.noreply.github.com>
2014-06-18 02:08:57 -0400

    Update NeuralNet.swift

--- NeuralNet.swift
+++ NeuralNet.swift
@@ -1,6 +1,13 @@
 // Creating a Neural Network from scratch
 
 import Foundation
+
+// Create exponents with '**'
+operator infix ** {}
+func ** (num: Double, power: Double) -> Double{
+    return pow(num, power)
+}
+
 func randomFunc(a: Double, b:Double) -> (Double) {
     var randNum = arc4random_uniform(101)
     var output = (b-a)*Double(randNum) + (a)
@@ -14,4 +21,14 @@ func randomFunc(I:Double, J:Double)->(ArrayType){
         m.append([fill]*J)
     return m
     }
+}
+
+// Sigmoid function. Later, will add more options for standard 1/(1+e^-x)
+func sigmoid(x: Double)->(Double){
+    return tanh(x)
+}
+
+// Derivative of our sigmoid function
+func dsigmoid(x: Double)->(Double){
+    return 1.0 - x**2.0
 }
